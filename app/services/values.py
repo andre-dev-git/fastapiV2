75067701839ec73d44340f81841d8116c23abfea
AndreCarvalhoLydia <131197835+AndreCarvalhoLydia@users.noreply.github.com>
2025-02-02 17:26:17 -0300

    trazendo de forma completa a lista de items

--- app/services/values.py
+++ app/services/values.py
@@ -1,11 +1,10 @@
 from typing import List
 from sqlalchemy.orm import Session
-from ..models import ItemValue
+from ..models import ItemValue, DynamicColumn, Item
 from ..schemas import ItemValueCreate
 
 class ValuesService:
     
-    # Método para criar um único valor
     def create_value(self, db: Session, value: ItemValueCreate):
         new_value = ItemValue(
             item_id=value.item_id,
@@ -17,11 +16,29 @@ class ValuesService:
         db.refresh(new_value)
         return new_value
     
-    # Método para criar múltiplos valores
     def create_values(self, db: Session, values: List[ItemValueCreate]):
         for value in values:
-            self.create_value(db, value)  # Agora estamos chamando create_value com self
+            self.create_value(db, value)
 
-    # Método para listar os valores
     def list_values(self, db: Session):
-        return db.query(ItemValue).all()
+        items = db.query(Item).all()
+        items_data = {item.id: [] for item in items}
+
+        values = db.query(ItemValue, DynamicColumn, Item).\
+            join(DynamicColumn, ItemValue.column_id == DynamicColumn.id).\
+            join(Item, ItemValue.item_id == Item.id).\
+            all()
+
+        for value in values:
+            item_value: ItemValue = value[0] 
+            dynamic_column: DynamicColumn = value[1]
+            item: Item = value[2]
+            
+            items_data[item.id].append({
+                "column_id": dynamic_column.id,
+                "column_name": dynamic_column.name,
+                "value": item_value.value
+            })
+
+        return items_data
+  
