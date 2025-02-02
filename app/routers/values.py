17e0e0bf04e939792897cd00b004a0c3b06fb7a9
AndreCarvalhoLydia <131197835+AndreCarvalhoLydia@users.noreply.github.com>
2025-02-02 00:05:35 -0300

    melhorias no processo de cadastrar novos valores

--- app/routers/values.py
+++ app/routers/values.py
@@ -1,3 +1,4 @@
+from typing import List
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 from ..database import get_db
@@ -8,9 +9,14 @@ from ..services.values import ValuesService
 router = APIRouter()
 
 @router.post("/item-values/")
-def add_item_value(value: ItemValueCreate, db: Session = Depends(get_db)):
-    return {"message": "Valor adicionado com sucesso!", "item_value": ValuesService.create_value(db, value)}
+def add_item_values(values: List[ItemValueCreate], db: Session = Depends(get_db)):
+    values_service = ValuesService()
+
+    values_service.create_values(db=db, values=values)
+    
+    return {"message": "Valores adicionados com sucesso!"}
 
 @router.get("/item-values/")
 def list_item_values(db: Session = Depends(get_db)):
-    return {"message": "Ok", "data": ValuesService.list_values(db)}
+    values_service = ValuesService()
+    return {"message": "Ok", "data": values_service.list_values(db)}
--- app/services/values.py
+++ app/services/values.py
@@ -1,9 +1,12 @@
+from typing import List
 from sqlalchemy.orm import Session
 from ..models import ItemValue
 from ..schemas import ItemValueCreate
 
 class ValuesService:
-    def create_value(db: Session, value: ItemValueCreate):
+    
+    # Método para criar um único valor
+    def create_value(self, db: Session, value: ItemValueCreate):
         new_value = ItemValue(
             item_id=value.item_id,
             column_id=value.column_id,
@@ -13,6 +16,12 @@ class ValuesService:
         db.commit()
         db.refresh(new_value)
         return new_value
+    
+    # Método para criar múltiplos valores
+    def create_values(self, db: Session, values: List[ItemValueCreate]):
+        for value in values:
+            self.create_value(db, value)  # Agora estamos chamando create_value com self
 
-    def list_values(db: Session):
+    # Método para listar os valores
+    def list_values(self, db: Session):
         return db.query(ItemValue).all()
