a587aa0796e582b0656936a8530978bea9c78859
AndreCarvalhoLydia <131197835+AndreCarvalhoLydia@users.noreply.github.com>
2025-02-03 13:36:03 -0300

    att de cors, pra conectar no front end

--- app/main.py
+++ app/main.py
@@ -1,11 +1,26 @@
 from fastapi import FastAPI
 from .routers import columns, items, values
 from .database import Base, engine
+from fastapi.middleware.cors import CORSMiddleware
 
 # Criar as tabelas no banco de dados
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+origins = [
+    "http://localhost:4200",  # Permite requisições do Angular
+    "http://127.0.0.1:4200"   # Outra forma de acessar localmente
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,  # Permitir essas origens
+    allow_credentials=True,
+    allow_methods=["*"],  # Permitir todos os métodos (GET, POST, etc.)
+    allow_headers=["*"],  # Permitir todos os headers
+)
+
 
 # Incluir as rotas
 app.include_router(columns.router, prefix="/api", tags=["Columns"])
